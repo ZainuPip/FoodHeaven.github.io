9cd3b33f0b1d05a5842d87190869b6a40b8c2850
ZainuPip <76224829+ZainuPip@users.noreply.github.com>
2020-12-19 00:06:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var num=4;//Math.floor(Math.random()*4)+1;
+
+$("#card").wScratchPad({
+  size        : 100,          // The size of the brush/scratch.
+  bg          : 'images/Scrath Image.jpg',  // Background (image path or hex color).
+  fg          : 'images/Scrath Image.jpg',  // Foreground (image path or hex color).
+  realtime    : true,       // Calculates percentage in realitime.
+  scratchDown : null,       // Set scratchDown callback.
+  scratchUp   : null,       // Set scratchUp callback.
+  scratchMove : null,       // Set scratcMove callback.
+  cursor      : 'pointer' // Set cursor.
+});
