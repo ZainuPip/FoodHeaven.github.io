2e5dfb7dd42a99e2fc9b2d6250a7dc31a64eb35c
ZainuPip <76224829+ZainuPip@users.noreply.github.com>
2020-12-19 12:50:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var num=Math.floor(Math.random()*4)+1;
 
 $("#card").wScratchPad({
   size        : 100,          // The size of the brush/scratch.
-  bg          : 'images/offer 1.jpg',  // Background (image path or hex color).
+  bg          : `images/offer ${num}.jpg`,  // Background (image path or hex color).
   fg          : 'images/Scrath Image.jpg',  // Foreground (image path or hex color).
   realtime    : true,       // Calculates percentage in realitime.
   scratchDown : null,       // Set scratchDown callback.
