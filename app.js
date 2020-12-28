7d127927d34fe59aa8df6c0f4dd10f8504157b65
ZainuPip <76224829+ZainuPip@users.noreply.github.com>
2020-12-28 11:25:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
-var num=Math.floor(Math.random()*4)+1;
+var num=Math.floor(Math.random()*3)+1;
 
 $("#card").wScratchPad({
   size        : 100,          // The size of the brush/scratch.
-  bg          : `images/offer ${num}.jpg`,  // Background (image path or hex color).
+  bg          : `image/silver/Offer ${num}.jpg`,  // Background (image path or hex color).
+  fg          : 'image/Logo.jpeg',  // Foreground (image path or hex color).
+  realtime    : true,       // Calculates percentage in realitime.
+  scratchDown : null,       // Set scratchDown callback.
+  scratchUp   : null,       // Set scratchUp callback.
+  scratchMove : null,       // Set scratcMove callback.
+  cursor      : 'pointer' // Set cursor.
+});
+
+
+$("#silver").wScratchPad({
+  size        : 100,          // The size of the brush/scratch.
+  bg          : `image/silver/Offer ${num}.jpg`,  // Background (image path or hex color).
+  fg          : 'image/Logo.jpeg',  // Foreground (image path or hex color).
+  realtime    : true,       // Calculates percentage in realitime.
+  scratchDown : null,       // Set scratchDown callback.
+  scratchUp   : null,       // Set scratchUp callback.
+  scratchMove : null,       // Set scratcMove callback.
+  cursor      : 'pointer' // Set cursor.
+});
+
+$("#gold").wScratchPad({
+  size        : 100,          // The size of the brush/scratch.
+  bg          : `image/gold/Offer ${num}.jpg`,  // Background (image path or hex color).
+  fg          : 'image/Logo.jpeg',  // Foreground (image path or hex color).
+  realtime    : true,       // Calculates percentage in realitime.
+  scratchDown : null,       // Set scratchDown callback.
+  scratchUp   : null,       // Set scratchUp callback.
+  scratchMove : null,       // Set scratcMove callback.
+  cursor      : 'pointer' // Set cursor.
+});
+
+$("#platinum").wScratchPad({
+  size        : 100,          // The size of the brush/scratch.
+  bg          : `image/platinum/Offer ${num}.jpg`,  // Background (image path or hex color).
   fg          : 'image/Logo.jpeg',  // Foreground (image path or hex color).
   realtime    : true,       // Calculates percentage in realitime.
   scratchDown : null,       // Set scratchDown callback.
